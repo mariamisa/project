91cba64bb787f8d84db12837b21b0ad93055ac2c
mariamisa <maryaam1998@hotmail.com>
2020-05-16 05:34:12 +0300

    errors

--- server/app.js
+++ server/app.js
@@ -6,11 +6,16 @@ const app= express();
 const router=require('./routes')
 const {client,server}=require('./controller')
 
+//middelwares
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
+
+//all routes
 app.use('/api/v1',router)
+//handel client error
 app.use(client)
+//hendel server error
 app.use(server)
 
 module.exports=app;
