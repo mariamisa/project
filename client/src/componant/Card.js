bac816f46b81ce30df8e292638ad1d3698233f60
mariamisa <maryaam1998@hotmail.com>
2020-05-18 00:56:28 +0300

    make search comp relates #24

--- client/src/componant/Card.js
+++ client/src/componant/Card.js
@@ -2,21 +2,20 @@ import React from "react";
 import './Card.css'
 
 const Card = (props) => {
-  const {item:{title,img,description,date,authorName}}= props;
-  return (
-      <div className="card">
-        <img src={`${img}`} 
-              alt="blog-img"
-              className="blog-container__img"/>
-        <div className="container">
-              <h2 className="blog-container__title">{title}</h2>
-              <p className="blog-container__date">{description}</p>
-              <p className="blog-container__text">{authorName}</p>
-              <p className="blog-container__media">{date}</p>
+    const {item:{title,img,description,date,authorName}}= props;
+    return (
+        <div className="card">
+            {/* <img src={`${img}`} 
+                alt="blog-img"
+                className="blog-container__img"
+            /> */}
+            <div className="container">
+                <h2 className="blog-container__title">{title}</h2>
+                <p className="blog-container__date">{description}</p>
+                <p className="blog-container__text">{authorName}</p>
+                <p className="blog-container__media">{date}</p>
+            </div>
         </div>
-      </div>
-
-  );
+    );
 };
-
 export default Card;
