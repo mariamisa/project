7e8395d1a3285d4d4fcdae2bf1f460c0b506ac22
mariamisa <maryaam1998@hotmail.com>
2020-05-20 04:23:58 +0300

    heroku

--- server/app.js
+++ server/app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const cookieParser=require('cookie-parser')
 const bodyParser=require('body-parser')
+const {join}=require('path')
 
 const app= express();
 
 const router=require('./routes')
 const {client,server}=require('./controller')
 
-//middelwares
+//middleware
 app.use(express.json());
 app.use(bodyParser.json({ extended: false }));
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static(join(__dirname, '..', 'client', 'build')));
 //all routes
 app.use('/api/v1',router)
 //handel client error
 app.use(client)
-//hendel server error
+//handel server error
 app.use(server)
 
 module.exports=app;
